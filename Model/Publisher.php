2df355877eacd1bced478a8d30c1e2baacfec703
Chaiwat S <iwatth@gmail.com>
2012-07-26 19:18:42 +0700

    validate Publisher

---
+++ Model/Publisher.php
@@ -0,0 +1,17 @@
+<?php
+class Publisher extends AppModel
+{
+	public $validate = array(
+		'name' => array(
+			'rule-1' => array(
+				'rule' => 'notEmpty',
+				'message' => 'This field must not be empty.'
+			),
+			'rule-2' => array(
+				'rule' => 'isUnique',
+				'message' => 'This field must be unique.'
+			)
+		)
+	);
+}
+
